7c4680dc235ea6ade7c08a05e12f766dbb37bfec
Sparsh Jain <77033352+Sparsh0810@users.noreply.github.com>
2022-02-23 23:21:47 +0530

    Create Friend_function.cpp

---
+++ Friend_function.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class Square;   // forward declaration
+
+class Rectangle
+{
+int width=5, height=6;
+public:
+  
+friend void display(Rectangle , Square );
+  
+};
+    class Square
+    {
+    int side=9;
+    public:
+    friend void display(Rectangle , Square );
+    };
+          void display(Rectangle r, Square s)
+          {
+          cout<<"Rectangle:"<< r.width * r.height;
+          cout<<"Square:"<< s.side * s.side;
+          }
+
+Object oriented programming with c++, Pranav Verma
+int main () 
+{
+Rectangle rec;
+Square sq;
+display(rec,sq);
+return 0;
+  
+}
