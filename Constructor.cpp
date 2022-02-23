5030d0f0dab2957db4101327c9d180cf2b9156bc
Sparsh Jain <77033352+Sparsh0810@users.noreply.github.com>
2022-02-23 23:30:02 +0530

    Create Constructor.cpp

---
+++ Constructor.cpp
@@ -0,0 +1,16 @@
+class Rectangle
+{
+int width,height;
+public:
+      Rectangle()
+      {
+      width=5;
+      height=6;
+      cout<<"Constructor Called";
+      }
+};
+int main()
+{
+     Rectangle r1;
+   return 0;
+}
